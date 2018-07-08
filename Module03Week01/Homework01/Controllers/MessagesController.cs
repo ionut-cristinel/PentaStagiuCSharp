e5e22c62056d433a4ba2260211b1d1526245aa66
ionut-cristinel <ionut.cristinel86@gmail.com>
2018-07-08 19:16:26 +0300

    Homework asp.net

---
+++ Module03Week01/Homework01/Controllers/MessagesController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Homework01.Models;
+
+namespace Homework01.Controllers
+{
+    public class MessagesController : Controller
+    {
+        public static List<Message> Messages = new List<Message>();
+        public static int Id = 0;
+
+        // GET: Messages
+        public ActionResult Index()
+        {
+            return View(Messages);
+        }
+
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            Message message = Messages.Find(m => m.Id == id);
+            if (message == null)
+                return HttpNotFound();
+            else
+                return View(message);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            Message message = Messages.Find(m => m.Id == id);
+            if (message == null)
+                return HttpNotFound();
+            else
+            {
+                Messages.Remove(message);
+                return RedirectToAction("Index");
+            }
+        }
+
+
+        //  partea de edit prezinta inca erori
+        //  ---------- ^ ---------  // 
+        [HttpGet]
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id)
+        {
+            Message message = Messages.Find(m => m.Id == id);
+            if (message == null)
+                return HttpNotFound();
+            else
+            {
+                var index = id - 1;
+                Messages[index] = message;
+                return RedirectToAction("Index");
+            }
+        }
+        //  ---------- ^ ---------  //
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Message message)
+        {
+            Random random = new Random();
+            Id++;
+            message.Id = Id;
+            message.TimeOfPosting = DateTime.Now.Date;
+            message.UserId = random.Next(1, 100);
+            message.PostType = (PostType)random.Next(0, 1);
+            Messages.Add(message);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Module03Week01/Homework01/Models/Message.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Homework01.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime TimeOfPosting { get; set; }
+        public string MessageContent { get; set; }
+        public PostType PostType { get; set; }
+        public bool IsSticky { get; set; }
+        public int Priority { get; set; }
+    }
+
+    public enum PostType
+    {
+        Text,
+        Photo
+    }
+}
+// Id(int)
+// UserId(int)
+// TimeOfPosting(DateTime)
+// Message(string)
+// PostType(Enum with 2 values: Text and Photo)
+// IsSticky(bool)
+// Priority(int – optional with values ranging from 1 to 5)
